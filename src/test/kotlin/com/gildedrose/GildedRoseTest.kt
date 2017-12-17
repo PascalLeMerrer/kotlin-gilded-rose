8b46b34aad660e67b4d4c084c4ef77cff1b209d7
Pascal Le Merrer <pascal.lemerrer@gmail.com>
2017-12-17 20:48:44 +0100

    Add golden master test

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,6 +1,8 @@
 package com.gildedrose
 
+import io.kotlintest.forAll
 import io.kotlintest.matchers.shouldBe
+import io.kotlintest.properties.forAll
 import io.kotlintest.specs.StringSpec
 
 class GildedRoseTests : StringSpec() {
@@ -11,6 +13,36 @@ class GildedRoseTests : StringSpec() {
             app.updateQuality()
             val item = app.items.get(0)
             item.name shouldBe "foo"
+        }
+
+
+        "new version should produce the same results than the legacy one " {
+            val items = arrayOf(Item("+5 Dexterity Vest", 10, 20), //
+                    Item("Aged Brie", 2, 0), //
+                    Item("Elixir of the Mongoose", 5, 7), //
+                    Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                    Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+            )
+            val app = GildedRose(items)
+
+            var legacyItems = emptyArray<Item>()
+
+            for(item in items) {
+                legacyItems = legacyItems.plus(item.copy())
+            }
+            
+            val legacyApp = LegacyGildedRose(legacyItems)
+
+            for (day in 0..999) {
+                app.updateQuality()
+                legacyApp.updateQuality()
+
+                for ((index, item) in items.withIndex()) {
+                    item.name shouldBe legacyItems.get(index).name
+                    item.quality shouldBe legacyItems.get(index).quality
+                    item.sellIn shouldBe legacyItems.get(index).sellIn
+                }
+            }
         }
     }
 }
